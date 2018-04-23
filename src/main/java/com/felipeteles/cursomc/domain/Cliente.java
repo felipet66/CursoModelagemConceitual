38c8f5da69f1e0955ceb4685d4c69cd1d7db0608
Unknown <fliptm95@gmail.com>
2018-04-22 22:24:28 -0300

    Pedido, EstadoPagamento e Pagamento
    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/felipeteles/cursomc/domain/Cliente.java
+++ src/main/java/com/felipeteles/cursomc/domain/Cliente.java
@@ -29,6 +29,9 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>(); 
@@ -129,5 +132,13 @@ public class Cliente implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 }
