666b09c5ee1a92ab838cd11ea3d9a03606d8de40
Unknown <fliptm95@gmail.com>
2018-04-23 01:28:37 -0300

    Validação sintatica com Bean Validation
    Validação sintatica com Bean Validation

--- src/main/java/com/felipeteles/cursomc/services/CategoriaService.java
+++ src/main/java/com/felipeteles/cursomc/services/CategoriaService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.felipeteles.cursomc.domain.Categoria;
+import com.felipeteles.cursomc.dto.CategoriaDTO;
 import com.felipeteles.cursomc.repositories.CategoriaRepository;
 import com.felipeteles.cursomc.services.exception.DataIntegrityException;
 
@@ -53,5 +54,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
