3821622fc199620ccbb10d3d1f32259c475326dc
Unknown <fliptm95@gmail.com>
2018-05-07 00:09:23 -0300

    Implementando Autorização

--- src/main/java/com/felipeteles/cursomc/config/SecurityConfig.java
+++ src/main/java/com/felipeteles/cursomc/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.felipeteles.cursomc.security.JWTAuthenticationFilter;
 import com.felipeteles.cursomc.security.JWTAuthorizationFilter;
 import com.felipeteles.cursomc.security.JWTUtil;
 
@@ -53,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
