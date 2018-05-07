f2ea54f00b3efe9ef30283ebce86705a601f69f1
Unknown <fliptm95@gmail.com>
2018-05-06 23:28:15 -0300

    Salvando Perfis de usuário na base de dados
    Salvando Perfis de usuário na base de dados

--- src/main/java/com/felipeteles/cursomc/services/DBService.java
+++ src/main/java/com/felipeteles/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.felipeteles.cursomc.domain.PagamentoComCartao;
 import com.felipeteles.cursomc.domain.Pedido;
 import com.felipeteles.cursomc.domain.Produto;
 import com.felipeteles.cursomc.domain.enums.EstadoPagamento;
+import com.felipeteles.cursomc.domain.enums.Perfil;
 import com.felipeteles.cursomc.domain.enums.TipoCliente;
 import com.felipeteles.cursomc.repositories.CategoriaRepository;
 import com.felipeteles.cursomc.repositories.CidadeRepository;
@@ -96,14 +97,20 @@ public class DBService {
 		
 		cli1.getTelefones().addAll(Arrays.asList("23758290", "978458493"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "bdavinis@gmail.com", "27139756031", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("9398921", "213458493"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38828019", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua Avenida Matos", "105", "Sala 800", "Centro", "38828019", cli1, c2);
+		Endereco e3 = new Endereco(null, "Rua Eduardo Avezos", "132", "Sala 2", "Centro", "3213211", cli2, c2);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
 		
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
@@ -138,8 +145,8 @@ public class DBService {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
